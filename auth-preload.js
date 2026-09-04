e2921e5941da50df00de3801741d0cb586215b12
christdembele00-droid <christdembele00@gmail.com>
2026-09-04 00:57:35 +0000

    Enable secure cross-origin authentication cookies

--- auth-preload.js
+++ auth-preload.js
@@ -3,12 +3,14 @@ const express=require("express");
 const {authRoutes,initAuthDb}=require("./auth");
 const originalListen=express.application.listen;
 let installed=false;
+const FRONTEND_ORIGIN=(process.env.FRONTEND_URL||"https://christdembele00-droid.github.io/Wiener-IA/").replace(/\/$/,"");
 express.application.listen=function(...args){
   if(!installed){
     installed=true;
     try{
       const router=this.router||this._router;
       const before=router?.stack?.length||0;
+      this.use((req,res,next)=>{const origin=req.headers.origin;if(origin===FRONTEND_ORIGIN){res.setHeader("Access-Control-Allow-Origin",origin);res.setHeader("Access-Control-Allow-Credentials","true");res.setHeader("Vary","Origin");}next();});
       authRoutes(this);
       const stack=router?.stack;
       if(stack&&stack.length>before){
